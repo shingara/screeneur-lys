191df18436701961ac2ab19f0337939177f2966a
shingara <shingara@10e25781-3034-0410-99f9-f5858d909610>
2007-07-22 17:32:57 +0000

    * Update default value
    git-svn-id: http://shingara.fr:8123/svn/code/ruby/rails/screeneur/trunk@46 10e25781-3034-0410-99f9-f5858d909610

--- db/migrate/022_add_map_base.rb
+++ db/migrate/022_add_map_base.rb
@@ -1,10 +1,10 @@
 class AddMapBase < ActiveRecord::Migration
   def self.up
     Map.create(:name => 'NoisRevii')
-    Map.create(:name => 'Debutant')
+    Map.create(:name => 'Le fleuve Tetovo')
   end
 
   def self.down
-    Map.destroy_all ['name IN (?,?)', 'NoisRevii', 'Debutant']
+    Map.destroy_all ['name IN (?,?)', 'NoisRevii', 'Debutant', 'Le fleuve Tetovo']
   end
 end
