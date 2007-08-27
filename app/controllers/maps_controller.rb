3a70b40fda8b09db8cd7d3acccc3780ea5f834b1
shingara <shingara@10e25781-3034-0410-99f9-f5858d909610>
2007-08-27 18:43:54 +0000

    == Screeneur
     * Add search player in map view
    
    
    git-svn-id: http://shingara.fr:8123/svn/code/ruby/rails/screeneur/trunk@89 10e25781-3034-0410-99f9-f5858d909610

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -28,4 +28,26 @@ class MapsController < ApplicationController
   rescue ActiveRecord::RecordNotFound
     render :status => 500
   end
+
+  # Search By Id Perso
+  def search
+    unless params[:perso].blank?
+      player = Player.find_by_lys_id params[:perso]
+      flash[:notice] = "Perso trouvé"
+      redirect_to :action => :index,
+        :x => player.box.x,
+        :y => player.box.y,
+        :step => params[:step],
+        :map_id => player.box.map.id,
+        :perso => params[:perso]
+    end
+  rescue ActiveRecord::RecordNotFound
+    flash[:notice] = "La recherche n'a rien donnée. Faite une nouvelle recherche"
+    redirect_to :action => :index, 
+      :x => params[:x], 
+      :y => params[:y],
+      :step => params[:step],
+      :map_id => params[:map_id],
+      :perso => params[:perso]
+  end
 end
