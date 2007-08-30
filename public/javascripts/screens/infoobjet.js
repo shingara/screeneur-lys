a7aa1197cd6c4e2c0cc11d7ef2f18667dafb2f55
shingara <shingara@10e25781-3034-0410-99f9-f5858d909610>
2007-08-30 21:00:25 +0000

    == Rails Screeneur
     * Fix bug in javascript where forgot unescape value
    
    
    git-svn-id: http://shingara.fr:8123/svn/code/ruby/rails/screeneur/trunk@98 10e25781-3034-0410-99f9-f5858d909610

--- public/javascripts/screens/infoobjet.js
+++ public/javascripts/screens/infoobjet.js
@@ -5,6 +5,6 @@ function infoobjet(ID,Libelle,Cible,Terrain,Bonus,Info){
 
   var expression = /(.+)Pos[^0-9]*([0-9]*\/[0-9]*)/
   expression.exec(Libelle)
-  $('objet').update(RegExp.$1)
-  $('objet_position').update(RegExp.$2)
+  $('objet').update(unescape(RegExp.$1))
+  $('objet_position').update(unescape(RegExp.$2))
 }
