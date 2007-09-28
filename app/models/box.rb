5aab53817c4d42d425d62af62f180f25d3bacfcc
shingara <shingara@10e25781-3034-0410-99f9-f5858d909610>
2007-09-28 17:13:24 +0000

    == Screeneur  * Add include in request for optimise request
    git-svn-id: http://shingara.fr:8123/svn/code/ruby/rails/screeneur/trunk@104 10e25781-3034-0410-99f9-f5858d909610

--- app/models/box.rb
+++ app/models/box.rb
@@ -20,7 +20,8 @@ class Box < ActiveRecord::Base
     all_y.each {|tmp_y|
       box_y[tmp_y] = Box.find(:all,
                               :conditions => ['x IN (?) AND y = ? AND map_id = ?', all_x, tmp_y, map_id],
-                              :order => 'x,y ASC')
+                              :order => 'x,y ASC',
+                              :include => :type)
     }
       [box_y, all_x.sort, all_y]
   end
