ede5919457d5329c999684dff4be0e8bbb7c614c
shingara <shingara@10e25781-3034-0410-99f9-f5858d909610>
2007-10-14 20:59:57 +0000

    = Screeneur =
     * Add information on new screeneur
    
    
    git-svn-id: http://shingara.fr:8123/svn/code/ruby/rails/screeneur/trunk@129 10e25781-3034-0410-99f9-f5858d909610

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
-  map.connect '', :controller => "screens", :action => "new"
+  map.welcome '', :controller => "screens", :action => "new"
   map.connect '/maps/:x/:y/:step/:map_id', :controller => "maps", :actions => "index"
 
   # Allow downloading Web Service WSDL as a file with an extension
